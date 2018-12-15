1a9737086aa475ec319bdf680a1590ac05d3c983
Leo Tanenbaum-Diaz <leo.tanenbaum.diaz@gmail.com>
2018-12-14 20:00:49 -0800

    fixed for mac os syscalls

--- src/ft_puts.s
+++ src/ft_puts.s
@@ -12,12 +12,9 @@
 _ft_puts:
 	mov		r11, rdi
 	call	_ft_strlen
-	mov		rdx, 6
-	mov		rax, 1		; ⎢  Set rax to determine which system call. (1 = write)
-	mov		rsi, msg	; ⎢  rsi = *ptr to the string
-	mov		rdi, 1		; ⎢  rdi = fd
+	mov		rdx, rax
+	mov		rax, 0x2000004		; ⎢  Set rax to determine which system call.
+	lea		rsi, [r11]			; ⎢  rsi = *ptr to the string
+	mov		rdi, 1				; ⎢  rdi = fd
 	syscall
 	ret
-
-			section		.data
-	msg		db			"Hello\n"
